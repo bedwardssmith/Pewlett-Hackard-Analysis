892e95e10d9af420d9d1cbd8d5e8a7ca100d4c71
Bev Edwards-Smith <bedwardssmith@yahoo.ca>
2021-02-07 14:32:16 -0500

    final analysis

--- Analysis_Projects_Folder/Pewlett_Hackard_Analysis_Folder/Queries/Employee_Database_challenge.sql
+++ Analysis_Projects_Folder/Pewlett_Hackard_Analysis_Folder/Queries/Employee_Database_challenge.sql
@@ -7,27 +7,44 @@ SELECT e.emp_no,
     t.to_date
 INTO retirement_titles
 FROM employees as e
-INNER JOIN titles as t
-ON (e.emp_no = t.emp_no)
+    INNER JOIN titles as t
+        ON (e.emp_no = t.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
 ORDER BY emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
-rt.first_name,
-rt.last_name,
-rt.title
+    rt.first_name,
+    rt.last_name,
+    rt.title
 INTO unique_titles
-FROM retirement_titles as rt
+    FROM retirement_titles as rt
 ORDER BY emp_no ASC, to_date DESC;
 
 -- Number of employees by their most recent job title who are about to retire
 SELECT COUNT (ut.emp_no), ut.title
 INTO retiring_titles
-FROM unique_titles as ut
+    FROM unique_titles as ut
 GROUP BY title 
 ORDER by count DESC
 
+-- Employees eligible to participate in a mentorship program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    de.from_date,
+    de.to_date,
+	ut.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as ut
+		ON (e.emp_no = ut.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+AND ut.to_date = ('9999-01-01')
+ORDER by emp_no;
 
 
 
